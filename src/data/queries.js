58e809583e23c011368fd737bf939479ebf9d554
/ | | |_| |_ <vipuls526@gmail.com>
2024-05-09 20:50:04 +0530

    Million row query

--- src/data/queries.js
+++ src/data/queries.js
@@ -1,9 +1,11 @@
 import queryResults1 from "./queryResults/queryResults1";
 import queryResults2 from "./queryResults/queryResults2";
 import queryResults3 from "./queryResults/queryResults3";
+import millionRowsQuery from "./queryResults/millionRowsQuery";
 
 export default {
     1: queryResults1,
     2: queryResults2,
     3: queryResults3,
+    4: millionRowsQuery,
 };
---
+++ src/data/queryResults/millionRowsQuery.js
@@ -0,0 +1,9 @@
+export default {
+    query: "SELECT * FROM big_table",
+    results: Array(1000000)
+        .fill(null)
+        .map((_, idx) => ({
+            UniqueID: idx,
+            CustomerName: `Name ${idx}`,
+        })),
+};
